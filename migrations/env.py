6411e82125f299ac70c62ce02cca352df336e3f8
mhfelix <mhfelix31@gmail.com>
2026-04-13 20:25:04 -0300

    fix(infra): corrected alembic env.py for test db injection

--- migrations/env.py
+++ migrations/env.py
@@ -30,7 +30,11 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", settings.database_url)
+url = config.get_main_option("sqlalchemy.url")
+
+if not url:
+    url = settings.database_url
+    config.set_main_option("sqlalchemy.url", url)
 
 
 def run_migrations_offline() -> None:
